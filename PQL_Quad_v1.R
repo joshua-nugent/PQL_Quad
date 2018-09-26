e4e10152ef23c8804f890cf6edd3a8a32eb8e5e5
Joshua Nugent <jnugent@Joshuas-MacBook-Pro.local>
2018-09-25 22:04:13 -0400

    Method is more flexible and begins to analyze output data. Need to work on better ways to input permuatations of different parameters.

--- PQL_Quad_v1.R
+++ PQL_Quad_v1.R
@@ -1,28 +1,19 @@
 library(lme4)
 library(MASS)
-
-# old: beta <- c(-2, 1.5, 0.5, -1)
+library(tidyverse)
 
 check_estimates <- function(n, p, b_0, b_1, sigbsq){
-  ## Initial tests: n <- 1500 p <- 3 b_0 <- -2 b_1 <- 1.5 sigbsq <- 4
-  
   betas <- c(b_0, b_1)
-
   # vector of p copies of each ID for 'long' data
   id <- rep(1:n, each = p)
-  
   # vector of 1111...0000 for treatment arms
   x1 <- as.numeric(id < (n+1)/2)
-  
   # Generate random normal values for error term, replicated p times for each ID
   randint <- rep(rnorm(n, 0, sqrt(sigbsq)), each = p)
-  
   # Data generation with specified model plus noise
   linpred <- b_0 + b_1*x1 + randint
-  
   # Invert t
   expit <- exp(linpred) / (1 + exp(linpred))
-  
   y <- runif(p*n) < expit
   
   # ? glmmML not needed anymore?
@@ -38,14 +29,15 @@ check_estimates <- function(n, p, b_0, b_1, sigbsq){
   (beta50 <- fixef(glmmres50))
   (beta100 <- fixef(glmmres100))
  
-  method <- c("True Value", "PQL", "Laplace", "nAGQ_10", "nAGQ_50", "nAGQ_100")
-  intercept_estimate <-c(b_0, betaPQL[1], betaLP[1], beta10[1], beta50[1], beta100[1])
-  x1_estimate <-c(b_1, betaPQL[2], betaLP[2], beta10[2], beta50[2], beta100[2])
+  value_labels <- c("n", "p", "b_0", "b_1", "sigbsq", 
+              "PQL_b_0", "Laplace_b_0", "nAGQ_10_b_0", "nAGQ_50_b_0", "nAGQ_100_b_0",
+              "PQL_b_1", "Laplace_b_1", "nAGQ_10_b_1", "nAGQ_50_b_1", "nAGQ_100_b_1")
+  estimates <-c(n, p, b_0, b_1, sigbsq,
+                betaPQL[1], betaLP[1], beta10[1], beta50[1], beta100[1],
+                betaPQL[2], betaLP[2], beta10[2], beta50[2], beta100[2])
 
-  results <- data.frame(method, intercept_estimate, x1_estimate)
-  names(results) <- c("Method", "Intercept Estimate", "Beta1 Estimate")
-  
-  return(results)
+  names(estimates) <- value_labels
+  return(estimates)
   
 ####  For later SE / coverage analysis 
 #  VcovPQL <- vcov(glmmresPQL, useScale = FALSE)
@@ -61,7 +53,93 @@ check_estimates <- function(n, p, b_0, b_1, sigbsq){
 #  (se100 <- sqrt(diag(Vcov100)))
 }
 
-check_estimates(1500, 3, -2, 1.5, 4)
-check_estimates(150, 3, -2, 1.5, 4)
-check_estimates(1500, 3, -2, 1.5, 1)
-check_estimates(1500, 3, -2, 1.5, 10)
+#check_estimates(1500, 3, -2, 1.5, 4)
+#check_estimates(150, 3, -2, 1.5, 4)
+#check_estimates(1500, 3, -2, 1.5, 1)
+#check_estimates(1500, 3, -2, 1.5, 10)
+
+n_s <- 900
+p_s <- 3
+b_0_s <- seq(from = -2, to = 2, by = .1)
+b_1_s <- seq(from = -2, to = 2, by = .1)
+b_1_s <- rev(b_1_s)
+sigbsq_s <- seq(from = 1, to = 5, by = .5)
+
+
+data <- as_tibble(t(mapply(check_estimates, n_s, p_s, b_0_s, b_1_s, sigbsq_s)))
+
+# Analyze bias of intercept (b_0) term
+data <- data %>% mutate(
+  b_0_PQL_diff = b_0 - PQL_b_0,
+  b_0_Laplace_diff = b_0 - Laplace_b_0,
+  b_0_10_diff = b_0 - nAGQ_10_b_0,
+  b_0_50_diff = b_0 - nAGQ_50_b_0,
+  b_0_100_diff = b_0 - nAGQ_100_b_0
+  )
+
+# Can add to list as model list changes
+diff_col_names_b_0 <- c(
+  "b_0_PQL_diff",
+  "b_0_Laplace_diff",
+  "b_0_10_diff",
+  "b_0_50_diff",
+  "b_0_100_diff"
+  )
+
+for (i in diff_col_names_b_0){
+  cat(sprintf("mean for %s", i))
+  cat(sprintf("is: \n"))
+  m <- mean(data[[i]])
+  print(m)
+}
+
+for (i in diff_col_names_b_0){
+  cat(sprintf("std dev for %s", i))
+  cat(sprintf("is: \n"))
+  sd <- sd(data[[i]])
+  print(sd)
+}
+
+for (i in diff_col_names_b_0){
+#  cat(sprintf("printing histogram for %s", i))
+  histogram <- hist(data[[i]], main = paste("Histogram of", i))
+}
+
+# Analyze bias of b_1 term
+data <- data %>% mutate(
+  b_1_PQL_diff = b_1 - PQL_b_1,
+  b_1_Laplace_diff = b_1 - Laplace_b_1,
+  b_1_10_diff = b_1 - nAGQ_10_b_1,
+  b_1_50_diff = b_1 - nAGQ_50_b_1,
+  b_1_100_diff = b_1 - nAGQ_100_b_1
+)
+
+
+# Can add to list as model list changes
+diff_col_names_b_1 <- c(
+  "b_1_PQL_diff",
+  "b_1_Laplace_diff",
+  "b_1_10_diff",
+  "b_1_50_diff",
+  "b_1_100_diff"
+)
+
+for (i in diff_col_names_b_1){
+  cat(sprintf("mean for %s", i))
+  cat(sprintf("is: \n"))
+  m <- mean(data[[i]])
+  print(m)
+}
+
+for (i in diff_col_names_b_1){
+  cat(sprintf("std dev for %s", i))
+  cat(sprintf("is: \n"))
+  sd <- sd(data[[i]])
+  print(sd)
+}
+
+for (i in diff_col_names_b_1){
+#  cat(sprintf("printing histogram for %s", i))
+  histogram <- hist(data[[i]], main = paste("Histogram of", i))
+}
+
