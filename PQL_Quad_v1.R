bc249bf4134a7ae22143a6ae6737052b98a7bd6f
Joshua Nugent <joshuanugent@gmail.com>
2018-09-30 21:32:53 -0400

    For doing small tests to make sure inputs are doing what they are supposed to

--- PQL_Quad_v1.R
+++ PQL_Quad_v1.R
@@ -18,96 +18,81 @@ check_estimates <- function(n, p, b_0, b_1, sigbsq){
   
   glmmresPQL <- glmmPQL(y ~ x1, random = ~ 1 | id , family = binomial(link = "logit"))
   glmmresLP <- glmer(y ~ x1 + (1|id), nAGQ = 1, family = binomial(link = "logit"))
+  glmmres4 <- glmer(y ~ x1 + (1|id), nAGQ = 4, family = binomial(link = "logit"))
   glmmres10 <- glmer(y ~ x1 + (1|id), nAGQ = 10, family = binomial(link = "logit"))
-  glmmres50 <- glmer(y ~ x1 + (1|id), nAGQ = 50, family = binomial(link = "logit"))
-  glmmres100 <- glmer(y ~ x1 + (1|id), nAGQ = 100, family = binomial(link = "logit"))
+  glmmres25 <- glmer(y ~ x1 + (1|id), nAGQ = 25, family = binomial(link = "logit"))
   
   betaPQL <- fixef(glmmresPQL)
   betaLP <- fixef(glmmresLP)
+  beta4 <- fixef(glmmres4)
   beta10 <- fixef(glmmres10)
-  beta50 <- fixef(glmmres50)
-  beta100 <- fixef(glmmres100)
+  beta25 <- fixef(glmmres25)
  
   value_labels <- c("n", "p", "b_0", "b_1", "sigbsq", 
-              "PQL_b_0", "Laplace_b_0", "nAGQ_10_b_0", "nAGQ_50_b_0", "nAGQ_100_b_0",
-              "PQL_b_1", "Laplace_b_1", "nAGQ_10_b_1", "nAGQ_50_b_1", "nAGQ_100_b_1")
+              "PQL_b_0", "Laplace_b_0", "nAGQ_4_b_0", "nAGQ_10_b_0", "nAGQ_25_b_0",
+              "PQL_b_1", "Laplace_b_1", "nAGQ_4_b_1", "nAGQ_10_b_1", "nAGQ_25_b_1")
   estimates <-c(n, p, b_0, b_1, sigbsq,
-                betaPQL[1], betaLP[1], beta10[1], beta50[1], beta100[1],
-                betaPQL[2], betaLP[2], beta10[2], beta50[2], beta100[2])
+                betaPQL[1], betaLP[1], beta4[1], beta10[1], beta25[1],
+                betaPQL[2], betaLP[2], beta4[2], beta10[2], beta25[2])
 
   names(estimates) <- value_labels
   return(estimates)
   
 ####  Might use this later for later SE / coverage analysis 
-#  VcovPQL <- vcov(glmmresPQL, useScale = FALSE)
-#  VcovLP <- vcov(glmmresLP, useScale = FALSE)
-#  Vcov10 <- vcov(glmmres10, useScale = FALSE)
-#  Vcov50 <- vcov(glmmres50, useScale = FALSE)
-#  Vcov100 <- vcov(glmmres100, useScale = FALSE)
-#  (sePQL <- sqrt(diag(VcovPQL)))
-#  (seLP <- sqrt(diag(VcovLP)))
-#  (se10 <- sqrt(diag(Vcov10)))
-#  (se50 <- sqrt(diag(Vcov50)))
-#  (se100 <- sqrt(diag(Vcov100)))
+#  VcovPQL <- vcov(glmmresPQL, useScale = FALSE) - get covariance matrix...
+# ...and use diagonal entries to get SE
+#  sePQL <- sqrt(diag(VcovPQL))
 }
-
-
-
 
 
 ###############################################################
 # Here we actually start running the simulation
 ###############################################################
 
+# Parameters to input to the generator...
+# We will explore what gives interesting results.
+n <- 40
+p <- 100
+b_0 <- -2
+b_1 <- 0.5
+sigbsq <- 1
 
-# Set up some random-ish vectors to input to the generator...
-# This part needs work - ask Ken - what parameters,
-# and how to permute them.
-n_s <- 900
-p_s <- 3
-b_0_s <- seq(from = -2, to = 2, by = .1)
-b_1_s <- seq(from = -2, to = 2, by = .1)
-b_1_s <- rev(b_1_s)
-sigbsq_s <- seq(from = 1, to = 5, by = .5)
+# KEY STEP - Output of the models
+data <- as_tibble(t(replicate(1000, check_estimates(n, p, b_0, b_1, sigbsq))))
 
-### KEY STEP - Output of the models ###
-data <- as_tibble(t(mapply(check_estimates, n_s, p_s, b_0_s, b_1_s, sigbsq_s)))
-
-
-# Add columns for bias of b_0 term...
-# ...this could be put into the function as well.
+# Add columns to our generator output to measure bias of b_0 term...
+# ...later, this could be put into the function as well.
 data <- data %>% mutate(
-  b_0_PQL_diff = b_0 - PQL_b_0,
-  b_0_Laplace_diff = b_0 - Laplace_b_0,
-  b_0_10_diff = b_0 - nAGQ_10_b_0,
-  b_0_50_diff = b_0 - nAGQ_50_b_0,
-  b_0_100_diff = b_0 - nAGQ_100_b_0
+  b_0_PQL_bias = b_0 - PQL_b_0,
+  b_0_Laplace_bias = b_0 - Laplace_b_0,
+  b_0_4_bias = b_0 - nAGQ_4_b_0,
+  b_0_10_bias = b_0 - nAGQ_10_b_0,
+  b_0_25_bias = b_0 - nAGQ_25_b_0
   )
-# Add columns for bias of b_1 term...
-# ...this could be put into the function as well.
+# Add columns to our generator output to measure bias of b_1 term...
 data <- data %>% mutate(
-  b_1_PQL_diff = b_1 - PQL_b_1,
-  b_1_Laplace_diff = b_1 - Laplace_b_1,
-  b_1_10_diff = b_1 - nAGQ_10_b_1,
-  b_1_50_diff = b_1 - nAGQ_50_b_1,
-  b_1_100_diff = b_1 - nAGQ_100_b_1
+  b_1_PQL_bias = b_1 - PQL_b_1,
+  b_1_Laplace_bias = b_1 - Laplace_b_1,
+  b_1_4_bias = b_1 - nAGQ_4_b_1,
+  b_1_10_bias = b_1 - nAGQ_10_b_1,
+  b_1_25_bias = b_1 - nAGQ_25_b_1
 )
 
 # Organizational structure as model list might grow
 # to hopefully avoid repeated code
 diff_col_names_b_0 <- c(
-  "b_0_PQL_diff",
-  "b_0_Laplace_diff",
-  "b_0_10_diff",
-  "b_0_50_diff",
-  "b_0_100_diff"
+  "b_0_PQL_bias",
+  "b_0_Laplace_bias",
+  "b_0_4_bias",
+  "b_0_10_bias",
+  "b_0_25_bias"
   )
 diff_col_names_b_1 <- c(
-  "b_1_PQL_diff",
-  "b_1_Laplace_diff",
-  "b_1_10_diff",
-  "b_1_50_diff",
-  "b_1_100_diff"
+  "b_1_PQL_bias",
+  "b_1_Laplace_bias",
+  "b_1_4_bias",
+  "b_1_10_bias",
+  "b_1_25_bias"
 )
 
 
@@ -127,7 +112,6 @@ for (i in diff_col_names_b_0){
 for (i in diff_col_names_b_0){
   histogram <- hist(data[[i]], main = paste("Histogram of intercept error of", i))
 }
-
 
 
 # Same as above, but for beta1 term
